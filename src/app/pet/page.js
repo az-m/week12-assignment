923f04af71765b3999b3813dce22b89f12d914b7
CLAIRE BOYLE <clairegboyle@gmail.com>
2025-07-14 15:32:19 +0100

    logic: necromancy pet buttons

--- src/app/pet/page.js
+++ src/app/pet/page.js
@@ -1,9 +1,11 @@
 import NaturePetButtons from "@/components/NaturePetButtons";
+import NecromancyPetButtons from "@/components/NecromancyPetButtons";
 
 export default function PetPage() {
   return (
     <>
       <NaturePetButtons />
+      <NecromancyPetButtons />
     </>
   );
 }
