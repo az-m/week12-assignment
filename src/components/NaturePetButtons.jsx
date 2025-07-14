8e923065998d9107071b0ff4f392861a22016a79
az-m <alisonzmills@gmail.com>
2025-07-14 16:18:30 +0100

    fix: fix for theme wrapper

--- src/components/NaturePetButtons.jsx
+++ src/components/NaturePetButtons.jsx
@@ -3,13 +3,10 @@
 import { interactPet } from "@/actions/naturepet";
 
 export default function NaturePetButtons() {
-  function handleClick(act) {
-    interactPet(act);
-  }
   return (
     <>
-      <button onClick={() => handleClick("feed")}>Feed</button>
-      <button onClick={() => handleClick("play")}>Play</button>
+      <button onClick={() => interactPet("feed")}>Feed</button>
+      <button onClick={() => interactPet("play")}>Play</button>
     </>
   );
 }
