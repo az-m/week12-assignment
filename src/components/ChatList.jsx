11d439b256b422e683ff6a6b14e18e991b3adc31
az-m <alisonzmills@gmail.com>
2025-07-14 10:52:27 +0100

    feat: refactored chat list

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -1,11 +1,17 @@
 import { db } from "@/utils/dbConnection";
 
-async function ChatList() {
-  const topChat = (
-    await db.query(
-      `SELECT post_id, text, first_name, family_name, created_at FROM posts JOIN student ON posts.student_id = student.student_id WHERE parent_post_id IS NULL ORDER BY created_at DESC`
-    )
-  ).rows;
+async function ChatList({ parentId = null }) {
+  let query;
+  let args = [];
+
+  if (parentId) {
+    query = `SELECT post_id, text, first_name, family_name, created_at FROM posts JOIN student ON posts.student_id = student.student_id WHERE parent_post_id = $1`;
+    args = [parentId];
+  } else {
+    query = `SELECT post_id, text, first_name, family_name, created_at FROM posts JOIN student ON posts.student_id = student.student_id WHERE parent_post_id IS NULL ORDER BY created_at DESC`;
+  }
+
+  const topChat = (await db.query(query, args)).rows;
 
   return (
     <>
@@ -19,35 +25,7 @@ async function ChatList() {
             <div>
               <p>{chat.text}</p>
             </div>
-            <ChatReplies parentId={chat.post_id} />
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-}
-
-async function ChatReplies({ parentId }) {
-  const replies = (
-    await db.query(
-      `SELECT post_id, text, first_name, family_name, created_at FROM posts JOIN student ON posts.student_id = student.student_id WHERE parent_post_id = $1`,
-      [parentId]
-    )
-  ).rows;
-
-  return (
-    <>
-      <ul>
-        {replies.map((reply) => (
-          <li key={reply.post_id}>
-            <div>
-              <span>{`${reply.first_name} ${reply.family_name}`}</span>
-              <span>{parseDate(reply.created_at)}</span>
-            </div>
-            <div>
-              <p>{reply.text}</p>
-            </div>
-            <ChatReplies parentId={reply.post_id} />
+            <ChatList parentId={chat.post_id} />
           </li>
         ))}
       </ul>
