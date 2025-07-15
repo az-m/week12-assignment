475b5bbbcabbb66d67e257a4ae5f72e0bb48f6f4
CLAIRE BOYLE <clairegboyle@gmail.com>
2025-07-15 15:00:40 +0100

    components: renamed footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server"; //the auth object contains all user's data eg userid
+import "./components.css";
+
+export default async function Footer() {
+  const { userId } = await auth();
+
+  // console.log(userId)
+
+  return (
+    <div className="footerFlex">
+      <Navbar />
+      <UserButton />
+    </div>
+  );
+}
+
+//we can delete the signed in tool from clerk here, as the footer only displays on sign-in anyway, so having additional conditional rendering is redundant
