bbef6743c90ae5bfe063461c5f0178aebaeb6498
az-m <alisonzmills@gmail.com>
2025-07-16 16:23:34 +0100

    ui: styled teacher pet console

--- src/components/TeacherPetButtons.jsx
+++ src/components/TeacherPetButtons.jsx
@@ -4,25 +4,31 @@ import { rewardPet } from "@/actions/teacherpet";
 export default function TeacherPetButtons({ natPet, necroPet, styles }) {
   console.log(natPet);
   return (
-    <>
-      <h2>House Nature</h2>
-      <section className="">
-        <button className="" onClick={() => rewardPet(3, "ribbon")}>
+    <div className={styles.teacherOptions}>
+      <h2 className={styles.nathead}>House Nature</h2>
+      <section className={styles.awards}>
+        <button
+          className={styles.button}
+          onClick={() => rewardPet(3, "ribbon")}
+        >
           {natPet.acc_ribbon ? "Remove Ribbon" : "Give Ribbon"}
         </button>
-        <button className="" onClick={() => rewardPet(3, "party")}>
+        <button className={styles.button} onClick={() => rewardPet(3, "party")}>
           {natPet.acc_party ? "Stop Party" : "Throw Party"}
         </button>
       </section>
-      <h2>House Necromancy</h2>
-      <section className="">
-        <button className="" onClick={() => rewardPet(4, "ribbon")}>
+      <h2 className={styles.necrohead}>House Necromancy</h2>
+      <section className={styles.awards}>
+        <button
+          className={styles.button}
+          onClick={() => rewardPet(4, "ribbon")}
+        >
           {necroPet.acc_ribbon ? "Remove Ribbon" : "Give Ribbon"}
         </button>
-        <button className="" onClick={() => rewardPet(4, "party")}>
+        <button className={styles.button} onClick={() => rewardPet(4, "party")}>
           {necroPet.acc_party ? "Stop Party" : "Throw Party"}
         </button>
       </section>
-    </>
+    </div>
   );
 }
