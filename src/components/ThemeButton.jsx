bf5f70dba5edddbc268607ec62c215cd47f794ac
az-m <alisonzmills@gmail.com>
2025-07-17 10:04:38 +0100

    ui: addded picture to profile page and styled classes

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,11 +1,8 @@
-
-
-
+//! THIS COMPONENT IS DEPRECATED
 
 "use client";
 
 import { useTheme } from "@/context/ThemeContext"; // custom hook
-
 
 export default function ThemeButton() {
   const { state, dispatch } = useTheme();
@@ -13,25 +10,33 @@ export default function ThemeButton() {
   return (
     <div className="flex gap-2">
       <button
-        className={state.theme === "light" ? "bg-blue-500 text-white px-3 py-1 rounded" : "bg-gray-200 text-black px-3 py-1 rounded"}
-
-       
+        className={
+          state.theme === "light"
+            ? "bg-blue-500 text-white px-3 py-1 rounded"
+            : "bg-gray-200 text-black px-3 py-1 rounded"
+        }
         onClick={() => dispatch({ type: "light" })}
       >
         Light Mode
       </button>
 
       <button
-        className={state.theme === "dark" ? "bg-blue-500 text-white px-3 py-1 rounded" : "bg-gray-200 text-black px-3 py-1 rounded"}
-    
+        className={
+          state.theme === "dark"
+            ? "bg-blue-500 text-white px-3 py-1 rounded"
+            : "bg-gray-200 text-black px-3 py-1 rounded"
+        }
         onClick={() => dispatch({ type: "dark" })}
       >
         Dark Mode
       </button>
 
       <button
-        className={state.theme === "dyslexia" ? "bg-blue-500 text-white px-3 py-1 rounded" : "bg-grey-200 text-yellow px-3 py-1 rounded"}
-       
+        className={
+          state.theme === "dyslexia"
+            ? "bg-blue-500 text-white px-3 py-1 rounded"
+            : "bg-grey-200 text-yellow px-3 py-1 rounded"
+        }
         onClick={() => dispatch({ type: "dyslexia" })}
       >
         Dyslexia Font
